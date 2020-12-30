824d6b182ee68852059109979d90391ea8c66269
MHThomsen <48956638+MHThomsen@users.noreply.github.com>
2020-12-30 20:43:12 +0100

    Add files via upload

---
+++ 02456_Table_Detection/RPN_custom.py
@@ -0,0 +1,91 @@
+from collections import OrderedDict
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torchvision
+from torchvision.ops import misc as misc_nn_ops
+from torchvision.ops import MultiScaleRoIAlign
+
+from torchvision.models.utils import load_state_dict_from_url
+
+from torchvision.models.detection.anchor_utils import AnchorGenerator
+from torchvision.models.detection.generalized_rcnn import GeneralizedRCNN
+#from torchvision.models.detection.rpn import RPNHead, RegionProposalNetwork
+from torchvision.models.detection.roi_heads import RoIHeads
+from torchvision.models.detection.transform import GeneralizedRCNNTransform
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+
+from RPN_generalized_rcnn import GeneralizedRPN
+from RPN_rpn import RPNHead,RegionProposalNetwork
+
+
+
+class RPN_custom(GeneralizedRPN):
+   
+
+    def __init__(self, backbone, num_classes=None,
+                 # transform parameters
+                 min_size=800, max_size=1333,
+                 image_mean=None, image_std=None,
+                 # RPN parameters
+                 rpn_anchor_generator=None, rpn_head=None,
+                 rpn_pre_nms_top_n_train=2000, rpn_pre_nms_top_n_test=1000,
+                 rpn_post_nms_top_n_train=2000, rpn_post_nms_top_n_test=1000,
+                 rpn_nms_thresh=0.7,
+                 rpn_fg_iou_thresh=0.7, rpn_bg_iou_thresh=0.3,
+                 rpn_batch_size_per_image=256, rpn_positive_fraction=0.5,
+                 # Box parameters
+                 box_roi_pool=None, box_head=None, box_predictor=None,
+                 box_score_thresh=0.05, box_nms_thresh=0.5, box_detections_per_img=100,
+                 box_fg_iou_thresh=0.5, box_bg_iou_thresh=0.5,
+                 box_batch_size_per_image=512, box_positive_fraction=0.25,
+                 bbox_reg_weights=None):
+
+        if not hasattr(backbone, "out_channels"):
+            raise ValueError(
+                "backbone should contain an attribute out_channels "
+                "specifying the number of output channels (assumed to be the "
+                "same for all the levels)")
+
+        assert isinstance(rpn_anchor_generator, (AnchorGenerator, type(None)))
+        assert isinstance(box_roi_pool, (MultiScaleRoIAlign, type(None)))
+
+        if num_classes is not None:
+            if box_predictor is not None:
+                raise ValueError("num_classes should be None when box_predictor is specified")
+        else:
+            if box_predictor is None:
+                raise ValueError("num_classes should not be None when box_predictor "
+                                 "is not specified")
+
+        out_channels = backbone.out_channels
+
+        if rpn_anchor_generator is None:
+            anchor_sizes = ((32,), (64,), (128,), (256,), (512,))
+            aspect_ratios = ((0.5, 1.0, 2.0),) * len(anchor_sizes)
+            rpn_anchor_generator = AnchorGenerator(
+                anchor_sizes, aspect_ratios
+            )
+        if rpn_head is None:
+            rpn_head = RPNHead(
+                out_channels, rpn_anchor_generator.num_anchors_per_location()[0]
+            )
+
+        rpn_pre_nms_top_n = dict(training=rpn_pre_nms_top_n_train, testing=rpn_pre_nms_top_n_test)
+        rpn_post_nms_top_n = dict(training=rpn_post_nms_top_n_train, testing=rpn_post_nms_top_n_test)
+
+        rpn = RegionProposalNetwork(
+            rpn_anchor_generator, rpn_head,
+            rpn_fg_iou_thresh, rpn_bg_iou_thresh,
+            rpn_batch_size_per_image, rpn_positive_fraction,
+            rpn_pre_nms_top_n, rpn_post_nms_top_n, rpn_nms_thresh)
+
+        if image_mean is None:
+            image_mean = [0.485, 0.456, 0.406]
+        if image_std is None:
+            image_std = [0.229, 0.224, 0.225]
+        transform = GeneralizedRCNNTransform(min_size, max_size, image_mean, image_std)
+
+        super(RPN_custom, self).__init__(backbone, rpn, transform)
+
---
+++ 02456_Table_Detection/faster-rcnn.py
@@ -0,0 +1,300 @@
+import pandas as pd
+import sys
+import numpy as np
+import os
+import json
+import re
+import torch
+import ast
+import cv2
+import io
+import pickle
+from PIL import Image
+import torchvision
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.rpn import AnchorGenerator
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+from torchvision.transforms import functional as F
+import engine, transforms, utils, coco_eval, coco_utils
+from engine import train_one_epoch, evaluate
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+from RPN_custom import RPN_custom
+
+
+
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+def resize_bbox(image_shape, newsize, bbox):
+    x1, y1, w, h = bbox
+    w_conv = newsize[0]/image_shape[0]
+    h_conv = newsize[1]/image_shape[1]
+    (x1, y1, w, h) = (x1*w_conv, y1*h_conv, w*w_conv, h*h_conv)
+    return x1, y1, w, h
+
+images_path = 'Detection/images/'
+
+class TableBank():
+    def __init__(self,images_path, Train, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        if Train:
+            self.annotations = Annotations_train
+        else:
+            self.annotations = Annotations_test
+        self.images_path = images_path
+        #self.transform = transform
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_path = os.path.join(self.images_path,
+                                self.annotations.iloc[idx]['filename'])
+        
+        image = Image.open(img_path).convert("RGB")
+        image_shape = image.size
+        newsize = (550,700)
+        image = image.resize(newsize,resample=Image.BILINEAR)
+        bboxes = ast.literal_eval(self.annotations.iloc[idx]['BoundingBoxes'])
+
+        #boxes = np.zeros((1,4), dtype=np.float32)
+        areas = []
+        boxes = []
+        for i in range(len(bboxes)):
+            (x1, y1, w, h) = resize_bbox(image_shape, newsize, bboxes[i])
+            x2 = x1+w
+            y2 = y1+h
+            boxes.append([x1,y1,x2,y2])
+            areas.append((h*w))
+            #if area < h*w:
+            #    area = h*w
+            #    boxes[0,:] = [x1,y1,x2,y2]
+
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # there is only one class
+        labels = torch.ones((len(boxes)), dtype=torch.int64)
+        #labels = torch.ones(1, dtype = torch.int64)
+        areas = torch.as_tensor(areas, dtype=torch.int64)
+        iscrowd = torch.zeros(len(boxes), dtype=torch.int64)
+        
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["image_id"] = torch.tensor([idx])
+        target["area"] = areas
+        target["iscrowd"] = iscrowd        
+
+        image = F.to_tensor(image)
+
+        return image, target
+
+Annotations_train = pd.read_csv('Detection/annotations/Annotations_train_sample.csv')
+Annotations_test = pd.read_csv('Detection/annotations/Annotations_test_sample.csv')
+
+#Annotations_train = pd.read_csv('Detection/annotations/ant_mini_train.csv')
+#Annotations_test = pd.read_csv('Detection/annotations/ant_mini_test.csv')
+
+
+TBdata = TableBank(images_path, Train=True)
+TBdata_test = TableBank(images_path,Train=False)
+
+
+
+def record_losses(model, data_loader, device, Ls,model_name):
+    with torch.no_grad():
+        #cpu_device = torch.device("cpu")
+        model.train()
+        #metric_logger = utils.MetricLogger(delimiter="  ")
+
+        if model_name == 'RPNresnet50':
+            record_losses = {'loss_objectness':0, 'loss_rpn_box_reg':0}
+        else:
+            record_losses = {'loss_classifier':0, 'loss_box_reg':0, 'loss_objectness':0, 'loss_rpn_box_reg':0}
+        counter = 0
+        for images, targets in data_loader:
+            images = list(image.to(device) for image in images)
+            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+            loss_dict = model(images, targets)
+            #print(loss_dict)
+            del images, targets
+            for k, v in loss_dict.items():
+                record_losses[k] += v
+            counter += 1
+
+        for k,v in record_losses.items():
+            record_losses[k] = v/counter
+        
+
+        losses = sum(loss for loss in record_losses.values())
+
+        Ls['total loss'].append(losses)
+        
+        Ls['loss_objectness'].append(record_losses['loss_objectness'])
+        Ls['loss_rpn_box_reg'].append(record_losses['loss_rpn_box_reg'])
+
+        if model_name !='RPNresnet50':
+            Ls['loss_classifier'].append(record_losses['loss_classifier'])
+            Ls['loss_box_reg'].append(record_losses['loss_box_reg'])
+    return Ls
+
+
+def main(network):
+    # train on the GPU or on the CPU, if a GPU is not available
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    # our dataset has two classes only - background and person
+    num_classes = 2
+
+    #dataset = torch.utils.data.Subset(TBdata,[range(len(TBdata))])
+    indices = torch.randperm(len(TBdata)).tolist()
+    dataset = torch.utils.data.Subset(TBdata, indices[:])
+    indices_ = torch.randperm(len(TBdata_test)).tolist()
+    dataset_val = torch.utils.data.Subset(TBdata_test, indices_[:])
+
+    # get the model using our helper function
+        #model = get_model_instance_segmentation(num_classes)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=8,
+                                sampler=None, num_workers=0, collate_fn=collate_fn)
+    dataloader_val = torch.utils.data.DataLoader(dataset_val, batch_size=8,
+                                sampler=None, num_workers=0, collate_fn=collate_fn)
+    
+    #Calculated statistics on training data:
+    #Transform parameters
+    min_size = 550  
+    max_size = 700  
+    image_means = [0.9492,0.9492,0.9492]
+    image_stds = [0.1158,0.1158,0.1158]
+
+    if network == 'resnet50':
+        backbone = resnet_fpn_backbone('resnet50', True)
+        model = FasterRCNN(backbone, num_classes, min_size=min_size, max_size=max_size, image_mean = image_means, image_std = image_stds)
+
+    elif network =='resnet18':
+        backbone = resnet_fpn_backbone('resnet18', True)
+        model = FasterRCNN(backbone, num_classes, min_size=min_size, max_size=max_size, image_mean = image_means, image_std = image_stds)
+        
+    elif network == 'resnet152':
+        backbone = resnet_fpn_backbone('resnet152', True)
+        model = FasterRCNN(backbone, num_classes, min_size=min_size, max_size=max_size, image_mean = image_means, image_std = image_stds)
+    
+    elif network == 'RPNresnet50':
+        backbone = resnet_fpn_backbone('resnet50', True)
+        model = RPN_custom(backbone, num_classes, min_size=min_size, max_size=max_size, image_mean = image_means , image_std = image_stds)
+    
+    elif network == 'RPNresnet152':
+        backbone = resnet_fpn_backbone('resnet152', True)
+        model = RPN_custom(backbone, num_classes, min_size=min_size, max_size=max_size, image_mean = image_means , image_std = image_stds)
+        
+
+    # move model to the right device
+    model.to(device)
+
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005,
+                                momentum=0.9, weight_decay=0.0005)
+    # and a learning rate scheduler
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=3,
+                                                   gamma=0.1)
+
+    
+    num_epochs = 10
+    Ls = {'total loss':[],'loss_classifier':[], 'loss_box_reg':[], 'loss_objectness':[], 'loss_rpn_box_reg':[]}
+    Ls_val = {'total loss':[],'loss_classifier':[], 'loss_box_reg':[], 'loss_objectness':[], 'loss_rpn_box_reg':[]}
+    
+    for epoch in range(num_epochs):
+        # train for one epoch, printing every 10 iterations
+        train_one_epoch(model, optimizer, dataloader, device, epoch, print_freq=10)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        #evaluate(model, dataloader_test, device=device)
+        Ls_val = record_losses(model, dataloader_val, device, Ls_val,network)
+
+        #record losses
+        Ls = record_losses(model, dataloader, device, Ls,network)
+    
+
+    #If folder does not exist already, create it
+    output_loc = f'./{network}/'
+
+    if not os.path.exists(output_loc):
+        os.makedirs(output_loc)
+    
+
+    
+    torch.save(model.state_dict(), output_loc+'model.pt')
+    
+
+    print("That's it!")
+    return Ls, Ls_val, num_epochs
+
+model_name = 'RPNresnet152'
+
+output_loc = f'./{model_name}/'
+
+Ls, Ls_val, num_epochs = main(model_name)
+
+
+for k, v in Ls.items():
+    for i in range(len(v)):
+        Ls[k][i] = v[i].cpu().numpy()
+
+with open(output_loc+"losses train dict {}.pickle".format(model_name), "wb") as handle:
+    pickle.dump(Ls, handle)
+
+
+for k, v in Ls_val.items():
+    for i in range(len(v)):
+        Ls_val[k][i] = v[i].cpu().numpy()
+
+with open(output_loc+"losses val dict {}.pickle".format(model_name), "wb") as handle:
+    pickle.dump(Ls_val, handle)
+
+#plotter kun hvis vi kører den fulde model. Plots kan vi altid bare lave igen alligevel
+if model_name != 'RPNresnet50':
+
+    import matplotlib.pyplot as plt
+
+    epoch = np.arange(num_epochs)
+    plt.figure()
+    plt.plot(epoch, Ls['total loss'], 'r', epoch, Ls['loss_classifier'], 'b', epoch, Ls['loss_box_reg'], 'g', epoch, Ls['loss_objectness'], 'c', epoch, Ls['loss_rpn_box_reg'], 'm')
+    plt.legend(['total loss','loss_classifier', 'loss_box_reg', 'loss_objectness', 'loss_rpn_box_reg'])
+    plt.xlabel('Epochs')
+    plt.ylabel('Training Loss {}'.format(model_name))
+    plt.savefig(output_loc+'Training Loss {}.png'.format(model_name))
+
+
+    plt.figure()
+    plt.plot(epoch, Ls_val['total loss'], 'r', epoch, Ls_val['loss_classifier'], 'b', epoch, Ls_val['loss_box_reg'], 'g', epoch, Ls_val['loss_objectness'], 'c', epoch, Ls_val['loss_rpn_box_reg'], 'm')
+    plt.legend(['total loss','loss_classifier', 'loss_box_reg', 'loss_objectness', 'loss_rpn_box_reg'])
+    plt.xlabel('Epochs')
+    plt.ylabel('Validation Loss {}'.format(model_name))
+    plt.savefig(output_loc+'Validation Loss {}.png'.format(model_name))
+
+
+    plt.figure()
+    plt.plot(epoch, Ls['total loss'] , 'r', epoch,Ls_val['total loss'],'b')
+    plt.legend(['Training loss','Validation loss'])
+    plt.xlabel('Epochs {}'.format(model_name))
+    plt.ylabel('Loss')
+    plt.savefig(output_loc+'Total losses {}.png'.format(model_name))
+
+
+
+#for loading model
+#https://pytorch.org/tutorials/beginner/saving_loading_models.html
