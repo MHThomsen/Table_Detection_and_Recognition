6a111ff01ab6cd8efc996d7b9f395277fc0f2be2
Jakob Vexø <jakobvexo@JakobVex.local>
2021-02-17 21:23:33 +0100

    computer vision boys

--- Table_Recognition/collapser_funcs.py
+++ Table_Recognition/collapser_funcs.py
@@ -1,18 +1,18 @@
 
 #En række funktioner som tager slice som input, og returnerer et 1d array med de ønskede features
-
+import torch
 
 class collapser_function():
     def __init__(self):
         self.out_dim = None
 
-    def is_correct_size(slice3d):
+    def is_correct_size(self, slice3d):
         #Expects input to be of shape CxWxH, but if not, 
         assert len(slice3d.shape) == 3, f"Slice er i {len(slice3d.shape)} dimensioner. Vi burde kun have et CxWxH input (ingen batch dimension)"    
         
 
 
-class min_collapser(collapser_function):
+class min_collapser():
 
     def __init__(self,
                  slice_channels,
@@ -20,12 +20,11 @@ class min_collapser(collapser_function):
                  slice_height):
         self.out_dim = slice_width*slice_height
     
-    def collapse(slice3d, *args):
-        super.is_correct_size(slice3d)
+    def collapse(self, slice3d, *args):
         return torch.min(slice3d,dim=0)[0].reshape(1,-1)
         
     
-class max_collapser(collapser_function):
+class max_collapser():
 
     def __init__(self,
                  slice_channels,
@@ -33,11 +32,10 @@ class max_collapser(collapser_function):
                  slice_height):
         self.out_dim = slice_width*slice_height
     
-    def collapse(slice3d, *args):
-        super.is_correct_size(slice3d)
+    def collapse(self, slice3d, *args):
         return torch.max(slice3d,dim=0)[0].reshape(1,-1)    
 
-class median_collapser(collapser_function):
+class median_collapser():
 
     def __init__(self,
                  slice_channels,
@@ -45,32 +43,27 @@ class median_collapser(collapser_function):
                  slice_height):
         self.out_dim = slice_width*slice_height
     
-    def collapse(slice3d, *args):
-        super.is_correct_size(slice3d)
+    def collapse(self, slice3d, *args):
         return torch.median(slice3d,dim=0)[0].reshape(1,-1)    
 
-class mean_channel_collapser(collapser_function):
-
-    def __init__(self,
-                 slice_channels,
-                 slice_width,
-                 slice_height):
-        
-         """
+class mean_channel_collapser():
+    """
         Calculate mean of all WxH pixels, across all channels. 
         Most intuitive; get mean of 2d slice
         """
         
+    def __init__(self,
+                 slice_channels,
+                 slice_width,
+                 slice_height):
         self.out_dim = slice_width*slice_height
-    
-    def collapse(slice3d, *args):
-        super.is_correct_size(slice3d)
-        
+
+    def collapse(self, slice3d, *args):        
         #OBS: intet [0] index på denne inden reshape. 
         return torch.mean(slice3d,dim=0).reshape(1,-1)   
 
 
-class mean_2d_collapser(collapser_function):
+class mean_2d_collapser():
 
     def __init__(self,
                  slice_channels,
@@ -85,14 +78,13 @@ class mean_2d_collapser(collapser_function):
         
         self.out_dim = slice_channels
     
-    def collapse(slice3d,*args):
-        super.is_correct_size(slice3d)
+    def collapse(self, slice3d,*args):
         
         #No need to reshape 
         return torch.mean(torch.mean(slice3d,dim=1),dim=1)  
 
     
-class slice_collapser(collapser_function):
+class slice_collapser():
 
     def __init__(self,
                  slice_channels,
@@ -101,11 +93,9 @@ class slice_collapser(collapser_function):
         
         self.out_dim = slice_width*slice_height
     
-    def collapse(slice3d,
+    def collapse(self, slice3d,
                  channel_no = 0):
-        
-        super.is_correct_size(slice3d)
-        
+                
         """
         Takes channel_no as input, determining which of the channels to extract
         """
--- Table_Recognition/gather.py
+++ Table_Recognition/gather.py
@@ -17,11 +17,62 @@ import numpy as np
 class simplest_gather():
     
     def __init__(self):
-        self.out_dim = 5
+        self.out_dim = 4
     
+    def gather(self,vertex_features,feature_map, collapser_function):
+        return vertex_features[:,:4]
+
+
+class slice_gather():
+
+    def __init__(self,collapser_function):
+        
+        self.collapser_func = collapser_function
+        self.out_dim = 4 + self.collapser_func.out_dim
+
     def gather(self,vertex_features,feature_map):
-        return vertex_features
+    
+        img_l, img_h = (768, 1366)
+
+        #x, y -dimensions of feature map
+        max_x, max_y = feature_map.shape[1], feature_map.shape[2]
+
+        l,h = 43.0, 16.0
+
+        #the height of a single pixel
+        #pxl_l, pxl_h = 1/feature_map.shape[1], 1/feature_map.shape[2]
+        
+        image_feats = []
+        for word in vertex_features:
+            #get x,y coordinates of word
+            x1, y1, x2, y2, _ = word.cpu().numpy()
+            #for this, we denormalize in order to utilize np.floor and np.ceil
+            x1, y1, x2, y2 = x1*img_l, y1*img_h, x2*img_l, y2*img_h 
+            #calculate centre of word
+            c = np.floor(((x1+(x2-x1)/2),(y1+(y2-y1)/2)))
+
+            #define x-coordinates of slice
+            x_slice = int(c[0]-np.floor(l/2)), int(c[0]+np.ceil(l/2))
+            if min(x_slice) < 0:
+                x_slice = (x_slice[0]+abs(min(x_slice)),x_slice[1]+abs(min(x_slice)))
+            if max(x_slice) > max_x:
+                x_slice = (x_slice[0]-(max(x_slice)-max_x),x_slice[1]-(max(x_slice)-max_x))
+
+            #define y-coordinates of slice
+            y_slice = int(c[1]-np.floor(h/2)),int(c[1]+np.ceil(h/2))
+            if min(y_slice) < 0:
+                y_slice = (y_slice[0]+abs(min(y_slice)),y_slice[1]+abs(min(y_slice)))
+            if max(y_slice) > max_y:
+                y_slice = (y_slice[0]-(max(y_slice)-max_y),y_slice[1]-(max(y_slice)-max_y))
+
+            #get 3D slice of word, flatten and concatenate with vertex features
+            slice_3d = feature_map[:,x_slice[0]:x_slice[1],y_slice[0]:y_slice[1]]
+
+            slice_collapsed = self.collapser_func.collapse(slice_3d)
+            image_feats.append(torch.cat((word[:4], slice_collapsed)))
+        return torch.stack(image_feats,dim=0)
 
 
+    
 
-
+'''        collapsus = collapser_function(channels,width,height) '''
